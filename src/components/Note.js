218e46b7a7c92ab849b810b4264f069c752388c6
chelosilvero78 <marcesilvero@gmail.com>
2021-12-02 15:05:28 -0300

    first commit

---
+++ src/components/Note.js
@@ -0,0 +1,23 @@
+import React from "react";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import Button from "@mui/material/Button";
+
+const Note = ({ id, text, date, handleDeleteNote }) => {
+  return (
+    <div className="note">
+      <div className="text-content">
+        <p className="text-item">
+          {text}
+        </p>
+      </div>
+      <div className="note-footer">
+        <small>{date}</small>
+        <Button onClick={() => handleDeleteNote(id)}>
+          <DeleteOutlineIcon className="delete-icon" size="1.3em" />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Note;
